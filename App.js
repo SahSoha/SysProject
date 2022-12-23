607550ff3466b071a3af08a0316764867153970f
Saif_Ashraf <112732757+SaifAshraf22@users.noreply.github.com>
2022-12-23 21:19:00 +0200

    Add files via upload
    my task:
    create home page with :
    1-lot of products
    2- write summary of our products and  website
    3-add back ground and pictures 
    4- many of features and actions

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/Homebage';
+
+function App() {
+  return (
+    <div className='App'>
+      <title>dsd</title>
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage>dd</HomePage>} />
+        </Routes>
+      </Router>
+
+
+
+    </div>
+  );
+}
+
+export default App;
