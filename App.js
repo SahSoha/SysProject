84fbdce514d9df5d01f0dd50eb22c425b465c7f6
eman <emanehab1273@gmail.com>
2022-12-24 00:28:23 +0200

    c

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/Homebage';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router><title>test</title>
         <Routes>
           <Route path='/' element={<HomePage>dd</HomePage>} />
+          <Route  path ='/login' element={<Login/>} />
         </Routes>
       </Router>
 
