f46e249d2d901c5dce8a97e5ff75e1ab423f644f
eman <emanehab1273@gmail.com>
2022-12-24 02:30:36 +0200

    #css

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/Homebage';
 import Login from './pages/Login';
+import './App.css';
+
 
 function App() {
   return (
