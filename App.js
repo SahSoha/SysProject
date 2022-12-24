89afaf9b1410cd790337e465cf9937ca52ea1503
Soha Ahmed <sohasrk14@gmail.com>
2022-12-24 18:54:33 +0200

    soha

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contact from './pages/Contact/Contact';
 import HomePage from './pages/HomePage/Homebage';
-import Login from './pages/Login';
-import './App.css';
+import Products from './pages/Products/Products';
 
 function App() {
-  document.title = 'FRAÎCHE';
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage>dd</HomePage>} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Products" element={<Products />} />
+          <Route path="/Contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
