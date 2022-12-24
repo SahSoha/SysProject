0afe28b5309845233b827d21fe449c3ef982d8b1
Soha Ahmed <sohasrk14@gmail.com>
2022-12-25 01:19:03 +0200

    s

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Contact from './pages/Contact/Contact';
-import HomePage from './pages/HomePage/Homebage';
-import Products from './pages/Products/Products';
-
+import Contact from './Pages/Contact/Contact';
+import HomePage from './Pages/HomePage/Homebage';
+import Products from './Pages/Products/Products';
 function App() {
   return (
     <div className="App">
--- frontend/src/Pages/HomePage/Homebage.js
+++ frontend/src/Pages/HomePage/Homebage.js
@@ -1,127 +1,156 @@
-import { Fragment } from "react";
-import { Link } from "react-router-dom";
-import Navbar from "../Navbar/Navbar";
-import "./HomePage.css";
-import Products from "../Products/Products";
-import Fotter from "../fotter/fotter";
-import Contact from "../Contact/Contact";
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
+import './HomePage.css';
+import Products from '../Products/Products';
+import Fotter from '../fotter/fotter';
+import Contact from '../Contact/Contact';
 
 function HomePage() {
   return (
-    <Fragment> 
-        
-<Navbar></Navbar>
+    <Fragment>
+      <Navbar></Navbar>
 
-{/*<!-- home section starts  --> */}
+      {/*<!-- home section starts  --> */}
 
-<section class="home" id="home">
+      <section class="home" id="home">
+        <div class="content">
+          <h3>healthy Milky.</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
+            sed! Qui maxime alias, porro dolorum explicabo inventore officia
+            nam! Ipsa nulla in accusamus cum ad deleniti laudantium assumenda
+            odit a.
+          </p>
+          <a href="#about">
+            <button class="btn">discover more</button>
+          </a>
+        </div>
+      </section>
 
-    <div class="content">
-        <h3>healthy Milky.</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, sed! Qui maxime alias, porro dolorum explicabo inventore officia nam! Ipsa nulla in accusamus cum ad deleniti laudantium assumenda odit a.</p>
-        <a href="#about"><button class="btn">discover more</button></a>
-    </div>
-
-</section>
-
-{/* <!-- home section ends -->
+      {/* <!-- home section ends -->
 
 <!-- product section starts  --> */}
 
-<Products></Products>
+      <Products></Products>
 
-{/* <!-- product section ends -->
+      {/* <!-- product section ends -->
 
 <!-- about section starts  --> */}
 
-<section class="about" id="about">
+      <section class="about" id="about">
+        <h1 class="heading">about us</h1>
 
-    <h1 class="heading">about us</h1>
+        <div class="row">
+          <div class="image">
+            <img
+              src="https://i.pinimg.com/originals/d2/b8/f5/d2b8f509aa8f39e8d8d1ade5a78cea04.jpg"
+              alt=""
+            />
+          </div>
 
-    <div class="row">
-
-        <div class="image">
-            <img src="https://i.pinimg.com/originals/d2/b8/f5/d2b8f509aa8f39e8d8d1ade5a78cea04.jpg" alt=""/>
-        </div>
-
-        <div class="content">
+          <div class="content">
             <h3>we are</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo quod voluptate debitis dolor beatae explicabo, est obcaecati distinctio, mollitia provident nostrum accusamus aspernatur quas, pariatur neque rem voluptas expedita temporibus.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo blanditiis ipsam iusto odio omnis illo cum necessitatibus officiis, iure laboriosam!</p>
-            <a href="#"><button class="btn">learn more</button></a>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+              quod voluptate debitis dolor beatae explicabo, est obcaecati
+              distinctio, mollitia provident nostrum accusamus aspernatur quas,
+              pariatur neque rem voluptas expedita temporibus.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+              blanditiis ipsam iusto odio omnis illo cum necessitatibus
+              officiis, iure laboriosam!
+            </p>
+            <a href="#">
+              <button class="btn">learn more</button>
+            </a>
+          </div>
         </div>
+      </section>
 
-    </div>
-
-</section>
-
-{/* <!-- about section ends -->
+      {/* <!-- about section ends -->
 
 <!-- service section starts  --> */}
 
-<section class="service" id="service">
+      <section class="service" id="service">
+        <h1 class="heading">our services</h1>
 
-    <h1 class="heading">our services</h1>
-
-    <div class="box-container">
-
-        <div class="box">
+        <div class="box-container">
+          <div class="box">
             <i class="fas fa-apple-alt"></i>
             <h3>organic food</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam excepturi voluptatem eos nulla iure dolorem porro expedita deserunt at ea.</p>
-        </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              excepturi voluptatem eos nulla iure dolorem porro expedita
+              deserunt at ea.
+            </p>
+          </div>
 
-        <div class="box">
+          <div class="box">
             <i class="fas fa-award"></i>
             <h3>high quality</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam excepturi voluptatem eos nulla iure dolorem porro expedita deserunt at ea.</p>
-        </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              excepturi voluptatem eos nulla iure dolorem porro expedita
+              deserunt at ea.
+            </p>
+          </div>
 
-        <div class="box">
+          <div class="box">
             <i class="fas fa-shipping-fast"></i>
             <h3>fast delivery</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam excepturi voluptatem eos nulla iure dolorem porro expedita deserunt at ea.</p>
-        </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              excepturi voluptatem eos nulla iure dolorem porro expedita
+              deserunt at ea.
+            </p>
+          </div>
 
-        <div class="box">
+          <div class="box">
             <i class="fas fa-undo"></i>
             <h3>easy returns</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam excepturi voluptatem eos nulla iure dolorem porro expedita deserunt at ea.</p>
-        </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              excepturi voluptatem eos nulla iure dolorem porro expedita
+              deserunt at ea.
+            </p>
+          </div>
 
-        <div class="box">
+          <div class="box">
             <i class="fas fa-truck"></i>
             <h3>free delivery</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam excepturi voluptatem eos nulla iure dolorem porro expedita deserunt at ea.</p>
-        </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              excepturi voluptatem eos nulla iure dolorem porro expedita
+              deserunt at ea.
+            </p>
+          </div>
 
-        <div class="box">
+          <div class="box">
             <i class="fas fa-headset"></i>
             <h3>24 x 7 support</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam excepturi voluptatem eos nulla iure dolorem porro expedita deserunt at ea.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              excepturi voluptatem eos nulla iure dolorem porro expedita
+              deserunt at ea.
+            </p>
+          </div>
         </div>
-        
-    </div>
+      </section>
 
-</section>
-
-{/* <!-- service section ends -->
+      {/* <!-- service section ends -->
 
 <!-- contact section starts  --> */}
-<Contact></Contact>
-{/* <!-- contact section ends -->
+      <Contact></Contact>
+      {/* <!-- contact section ends -->
 
 <!-- footer section  --> */}
-<Fotter></Fotter>
+      <Fotter></Fotter>
 
-{/* <!-- scroll top  --> */}
-<a href="#home" class="fas fa-arrow-up" id="scroll-top"></a>
-
-
-
-
-
-    </Fragment> 
-    );
-  }
-  export default HomePage;
+      {/* <!-- scroll top  --> */}
+      <a href="#home" class="fas fa-arrow-up" id="scroll-top"></a>
+    </Fragment>
+  );
+}
+export default HomePage;
