36dcf2d6c5000687adda027daa007347ff8fc222
Soha Ahmed <sohasrk14@gmail.com>
2022-12-29 07:03:01 +0200

    aaaa

---
+++ frontend/src/Pages/Cart/Cart.js
@@ -0,0 +1,75 @@
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import './Cart.css';
+
+function Cart() {
+  return (
+    <Fragment>
+      <div class="container1">
+        <div class="your cart">
+          <h1>your cart</h1>
+          <div class="summary_card">
+            <div class="card_item">
+              <div class="product_img">
+                <img src="https://cdn.gourmetegypt.com/media/catalog/product/cache/2b4d21b90ad5abb98380bc0a709a4ac8/6/2/6222014310294.jpg" />
+              </div>
+              <div class="product_info">
+                <h1>Milk</h1>
+                <p>Lorem Ipsum is simply dummy text.</p>
+                <div class="close-btn">
+                  <i class="fa fa-close"></i>
+                </div>
+                <div class="product_rate_info">
+                  <h1>$ 20</h1>
+                  <span class="pqt-minus">-</span>
+                  <span class="pqt">1</span>
+                  <span class="pqt-plus">+</span>
+                </div>
+              </div>
+            </div>
+            <div class="card_item">
+              <div class="product_img">
+                <img
+                  src="https://www.puckarabia.com/globalassets/new-products-pictures/packshots/resized/11-puck-cream-500g-ara.png?preset=product640"
+                  alt=""
+                />
+              </div>
+              <div class="product_info">
+                <h1>Cream Cheese</h1>
+                <p>Lorem Ipsum is simply dummy text.</p>
+                <div class="close-btn">
+                  <i class="fa fa-close"></i>
+                </div>
+                <div class="product_rate_info">
+                  <h1>$ 16</h1>
+                  <span class="pqt-minus">-</span>
+                  <span class="pqt">2</span>
+                  <span class="pqt-plus">+</span>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div class="order_price">
+              <p>Order summary</p>
+              <h4>$36</h4>
+            </div>
+            <div class="order_service">
+              <p>delivery</p>
+              <h4>$10</h4>
+            </div>
+            <div class="order_total">
+              <p>Total Amount</p>
+              <h4>$46</h4>
+            </div>
+            <div className="Checkout Button">
+              <Link to="/Checkout">
+                <button>Checkout</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+export default Cart;
--- frontend/src/Pages/Navbar/Navbar.js
+++ frontend/src/Pages/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-
+import { ShoppingCart } from 'phosphor-react';
 
 function Navbar() {
   return (
@@ -16,12 +16,17 @@ function Navbar() {
 
         <nav class="navbar">
           <title className="fra"></title>
-          <Link to="/" style={{ color: 'black' }}>FRAÎCHE</Link>
+          <Link to="/" style={{ color: 'black' }}>
+            FRAÎCHE
+          </Link>
           {/* <Link to="/">Home</Link> */}
           <Link to="/Products">product</Link>
           <Link to="/Contact">Contact us</Link>
           <Link to="/login">Login</Link>
           <Link to="/Profile">My Account</Link>
+          <Link to="/Cart">
+            <ShoppingCart size={28} />
+          </Link>
         </nav>
       </header>
     </Fragment>
